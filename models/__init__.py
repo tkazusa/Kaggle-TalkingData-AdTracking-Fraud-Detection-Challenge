78f4ef898fffb77ca45fc8a16e3aca76fe7c7bd2
Kazusa <tkazusa@users.noreply.github.com>
2019-04-22 16:25:46 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,3 @@
 # -*- encoding: UTF-8 -*-
-from abc import abstractmethod
-from typing import List
-
-
-class Model:
-    @abstractmethod
-    def train_and_predict(self, train, valid, categorical_features: List[str], target: str, params: dict):
-        raise NotImplementedError
+from .lightgbm import LightGBM
+from .base import Model
