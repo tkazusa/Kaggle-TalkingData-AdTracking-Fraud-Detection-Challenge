0d9f53ffa362501830beaa96ed2ddef20810f117
Kazusa <tkazusa@users.noreply.github.com>
2019-04-22 16:27:48 +0900

    Update base.py

--- models/base.py
+++ models/base.py
@@ -1,9 +1,13 @@
 # -*- encoding: UTF-8 -*-
-from abc import abstractclassmethod
+from abc import abstractmethod
 from typing import List
 
 
 class Model:
-    @abstractclassmethod
-    def train_and_predict(self, train, valid, categorical_features: List[str], target: str, params: dict):
+    @abstractmethod
+    def train_and_predict(self, train, valid, weight, categorical_features: List[str], target: str, params: dict):
+        raise NotImplementedError
+
+    @abstractmethod
+    def train_without_validation(self, train, weight, categorical_features: List[str], target: str, params: dict, best_iteration: int):
         raise NotImplementedError
