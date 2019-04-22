52395f5939645d41362b1311f6f1b3974b656fe0
Kazusa <tkazusa@users.noreply.github.com>
2019-04-22 15:47:09 +0900

    Update lightgbm.py

--- models/lightgbm.py
+++ models/lightgbm.py
@@ -36,3 +36,20 @@ class LightGBM(Model):
                           evals_result=eval_results,
                           **params['train_params'])
         return model, eval_results
+
+    
+    def train_without_validation(self, train, weight, categorical_features: List[str], target: str, params: dict, best_iteration: int):
+        predictors = train.columns.drop(target)
+        if weight is None:
+            d_train = lgb.Dataset(train[predictors], label=train[target].values)
+        else:
+            d_train = lgb.Dataset(train[predictors], label=train[target].values, weight=weight)
+        train_params = copy.deepcopy(params['train_params'])
+        train_params['num_boost_round'] = best_iteration
+        if 'early_stopping_rounds' in train_params:
+            del train_params['early_stopping_rounds']
+        model = lgb.train(params['model_params'],
+                          d_train,
+                          categorical_feature = categorical_features,
+                          **train_params)
+        return model
